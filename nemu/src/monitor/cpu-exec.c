a955e250b2c1a28591d324933a4785b075ab3a50
tracer-ics2017 <tracer@njuics.org>
2018-03-31 14:09:42 +0000

    >  compile 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  14:09:42 up  1:14,  1 user,  load average: 0.07, 0.02, 0.00

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -33,15 +33,10 @@ void cpu_exec(uint64_t n) {
 
 #ifdef DEBUG
     /* TODO: check watchpoints here. */
-	int temp=check_wp();
+
 	while(check_wp())
 	{
 		;
-	}
-	if(temp==1)
-	{
-		nemu_state=NEMU_STOP;
-		printf("STOP!");
 	}
 
 	if(check_bp())

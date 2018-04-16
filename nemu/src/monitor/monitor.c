5bfaffb2e57fd81c6dac4ce903f9c069be7e75ae
tracer-ics2017 <tracer@njuics.org>
2018-04-16 12:59:23 +0000

    >  compile 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  12:59:23 up  5:24,  1 user,  load average: 0.00, 0.00, 0.00 62d72e005b40e1fd5b53dba7e12323cecfec439

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -82,7 +82,7 @@ static inline void load_img() {
 static inline void restart() {
   /* Set the initial instruction pointer. */
   cpu.eip = ENTRY_START;
-
+  
 #ifdef DIFF_TEST
   init_qemu_reg();
 #endif

b17b0d0d38e776b71cab861912f798ad2e78c81a
tracer-ics2017 <tracer@njuics.org>
2018-04-18 03:50:24 +0000

    >  compile 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  03:50:24 up  1:44,  1 user,  load average: 0.00, 0.00, 0.00 6ff936dfa96326f13bd7c792ad5bf4f533cbe070

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -33,14 +33,14 @@ nemu_state = NEMU_RUNNING;
 
 #ifdef DEBUG
     /* TODO: check watchpoints here. */
-
+/*
 	if(check_wp())
 	{
 		nemu_state=NEMU_STOP;
 		printf("STOP!\n");
 		return;
 	}
-
+*/
 	if(check_bp())
 	{ 
 		nemu_state=NEMU_STOP;

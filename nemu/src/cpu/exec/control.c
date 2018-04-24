2360d9403c9ddaa6fa29d499b0af01168092dae6
tracer-ics2017 <tracer@njuics.org>
2018-04-24 15:19:48 +0000

    >  compile 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  15:19:48 up  4:16,  1 user,  load average: 0.01, 0.01, 0.00 ef3d2289cd49b9b6fae66e024e0f266624493bc7

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -8,9 +8,11 @@ make_EHelper(jmp) {
 }
 
 make_EHelper(jcc) {
+  rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
   // the target address is calculated at the decode stage
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
+  decoding.jmp_eip = decoding.seq_eip + id_dest->val;
   decoding.is_jmp = t2;
 
   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);

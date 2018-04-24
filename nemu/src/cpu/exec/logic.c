1d791de7817b9a42c813a71f09ade6c43cc42222
tracer-ics2017 <tracer@njuics.org>
2018-04-24 12:27:02 +0000

    >  compile 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  12:27:02 up  1:49,  1 user,  load average: 0.15, 0.08, 0.02 549f162f191492652a427395122abc705336f5

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -5,8 +5,9 @@ make_EHelper(test) {
   rtl_set_CF(&tzero);
   rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
   rtl_sext(&id_src->val, &id_src->val, id_src->width);
-  rtl_and(&t0, &id_dest->val, &id_src->val);
-  rtl_update_ZFSF(&t0, id_dest->width);
+  rtl_and(&t1, &id_dest->val, &id_src->val);
+  rtl_update_ZFSF(&t1, id_dest->width);
+  operand_write(id_dest, &t1);
 
   print_asm_template2(test);
 }

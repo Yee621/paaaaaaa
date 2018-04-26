9225f9f0c5ac359dd0b55cf7861cc5b6ad045b31
tracer-ics2017 <tracer@njuics.org>
2018-04-26 10:52:16 +0000

    >  compile 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  10:52:16 up  2:37,  1 user,  load average: 0.00, 0.00, 0.00 ff7cc86c2a7f791c647d7372bb96437b2168baa7

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -14,7 +14,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
+	case CC_O:
 		rtl_get_OF(dest);
 		break;
     case CC_B:

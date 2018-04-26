b8fba51eee98eb8afe40ef1e3ffbce78bd69baf1
tracer-ics2017 <tracer@njuics.org>
2018-04-26 10:16:29 +0000

    >  compile 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  10:16:29 up  2:01,  1 user,  load average: 0.04, 0.01, 0.00 b7a518142697c482b7a32e2d85f945a3e298ebbe

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -67,6 +67,8 @@ make_EHelper(inc) {
   rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
 
   rtl_addi(&t0, &id_dest->val, 1);
+  operand_write(id_dest, &t0);
+
   rtl_update_ZFSF(&t0, id_dest->width);
   rtl_sltu(&t1, &id_dest->val, &id_src->val);
   rtl_set_CF(&t1);
@@ -75,8 +77,6 @@ make_EHelper(inc) {
   rtl_and(&t2, &t2, &t1);
   rtl_msb(&t2, &t2, id_dest->width);
   rtl_set_OF(&t2);
-  
-  operand_write(id_dest, &t0);
 
   print_asm_template1(inc);
 }

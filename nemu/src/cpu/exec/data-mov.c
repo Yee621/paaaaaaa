c90b6eb7c755a84d300c23efd14e6ae349efd542
tracer-ics2017 <tracer@njuics.org>
2018-04-27 13:49:43 +0000

    >  compile 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  13:49:43 up 25 min,  1 user,  load average: 0.11, 0.04, 0.05 bf73e9a7ce9f763144f84f4e864743d43a225c3

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -62,6 +62,7 @@ make_EHelper(cwtl) {
 
 make_EHelper(movsx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
+  printf("%#x\n",id_src->val);
   rtl_sext(&t2, &id_src->val, id_src->width);
   operand_write(id_dest, &t2);
   print_asm_template2(movsx);

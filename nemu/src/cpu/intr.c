9c77027b35b1879cc42c8f9cb691109d5a83488b
tracer-ics2017 <tracer@njuics.org>
2018-05-24 11:15:23 +0000

    >  compile 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  11:15:23 up  5:48,  1 user,  load average: 0.01, 0.01, 0.00 af5c2d93700932f4f9e3305630c1c86b95aedfa9

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,10 +11,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push((rtlreg_t *)&ret_addr);
   uint32_t idtr_base = cpu.idtr.base;
   decoding.is_jmp = true;
-  uint32_t low, high;
-  low = vaddr_read(idtr_base + NO * 8, 4) & 0x0000ffff;
-  high = vaddr_read(idtr_base + NO * 8 + 4, 4) & 0xffff0000;
-  decoding.jmp_eip = (low | high);
+  decoding.jmp_eip = (vaddr_read(idtr_base + NO * 8, 4) & 0x0000ffff) | (vaddr_read(idtr_base + NO * 8 + 4, 4) & 0xffff0000);
 }
 
 void dev_raise_intr() {

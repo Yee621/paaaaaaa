4a8b283f3dc8b9e7fe59b6b8eb70a7be361914e4
tracer-ics2017 <tracer@njuics.org>
2018-05-24 11:28:15 +0000

    >  compile 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  11:28:15 up  6:01,  1 user,  load average: 0.01, 0.02, 0.00 771ade1b5b52a478a1dc0dd216a413e11d135a75

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -42,7 +42,10 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  TODO();
+  rtl_pop(&decoding.jmp_eip);
+  rtl_pop(&cpu.cs);
+  rtl_pop(&cpu.eflags.val);
+  decoding.is_jmp = 1;
 
   print_asm("iret");
 }

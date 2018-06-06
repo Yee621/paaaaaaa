71c217284a8662921aeb3c23c23e4b952a84dfa9
tracer-ics2017 <tracer@njuics.org>
2018-06-06 12:37:48 +0000

    >  run 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  12:37:48 up  4:43,  1 user,  load average: 0.00, 0.00, 0.00 c07fda2d245d438d8ecde1af4d1bad7ab8148141

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -33,6 +33,7 @@ int _write(int fd, void *buf, size_t count){
 
 void *_sbrk(intptr_t increment){
   //return (void *)-1;
+  Log("%d",increment);
   static void *program_break = (void *)&_end;
   if(_syscall_(SYS_brk, (intptr_t)program_break + increment, 0, 0) == 0){
 	  void *old_pb = program_break;

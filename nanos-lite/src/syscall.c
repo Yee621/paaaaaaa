40f45a6619c56e752d56068a3b0206b45a0094fd
tracer-ics2017 <tracer@njuics.org>
2018-06-06 11:44:41 +0000

    >  run 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  11:44:41 up  3:49,  1 user,  load average: 0.04, 0.03, 0.01 acfe1ae7e108cb89c57cb685d2484d0a8c1e8637

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -28,7 +28,7 @@ _RegSet* do_syscall(_RegSet *r) {
 		_halt(a[1]);
 		break;
 	case SYS_write:
-		Log("call the write\n");
+		Log();
 		r->eax = sys_write(a[1], (void *)a[2], a[3]);
 		//r->eax = fs_write(a[1], (void *)a[2], a[3]);
 		break;

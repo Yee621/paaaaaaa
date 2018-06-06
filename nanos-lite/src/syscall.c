4b11360f5018eb7fae4553ef5ab212449cde2e19
tracer-ics2017 <tracer@njuics.org>
2018-06-06 10:32:47 +0000

    >  run 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  10:32:47 up  2:38,  1 user,  load average: 0.00, 0.01, 0.00 c3509a5a4e17f49e16a878dd7457a4532a7609d9

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -18,7 +18,7 @@ _RegSet* do_syscall(_RegSet *r) {
 		_halt(a[1]);
 		break;
 	case SYS_write:
-		//Log("call the write\n");
+		Log("call the write\n");
 		r->eax = fs_write(a[1], (uint8_t *)a[2], a[3]);
 		break;
     default: panic("Unhandled syscall ID = %d", a[0]);

eaf716aa8575f64e4162b2e13277da8d0a3348ea
tracer-ics2017 <tracer@njuics.org>
2018-06-07 16:08:04 +0000

    >  run 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  16:08:04 up  7:04,  1 user,  load average: 0.10, 0.12, 0.05 964dadbac304dbaa60cf4e0d7315ce8a56b81bb4

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -3,25 +3,49 @@
 #define NAME(key) \
   [_KEY_##key] = #key,
 
+//extern void switch_game();
+
 static const char *keyname[256] __attribute__((used)) = {
   [_KEY_NONE] = "NONE",
   _KEYS(NAME)
 };
 
 size_t events_read(void *buf, size_t len) {
-  return 0;
+  //return 0;
+  int key = _read_key();
+  if(key == _KEY_NONE){
+	  unsigned long t = _uptime();
+	  sprintf(buf, "t %d\n", t);
+  }
+  else if(key & 0x8000){
+	  key ^= 0x8000;
+	  sprintf(buf, "kd %s\n", keyname[key]);
+	  //if(key == _KEY_F12)
+		  //switch_game();
+  }
+  else
+	  sprintf(buf, "ku %s\n", keyname[key]);
+  return strlen(buf);
 }
 
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
+	memcpy(buf, dispinfo + offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+	int row, col;
+	offset /= 4;
+	col = offset % _screen.width;
+	row = offset / _screen.width;
+	_draw_rect(buf, col, row, len/4, 1);
 }
 
 void init_device() {
   _ioe_init();
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", _screen.width, _screen.height);
+  Log("WIDTH:%d\nHEIGHT:%d\n", _screen.width, _screen.height);
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention

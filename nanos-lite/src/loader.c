2102f3790ba729a7eb671dba69e05fcb352e826d
tracer-ics2017 <tracer@njuics.org>
2018-06-07 09:50:43 +0000

    >  run 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  09:50:43 up  1:38,  1 user,  load average: 0.08, 0.06, 0.04 6e5cbfcf365dcd4be2916b5e65bc60c2a1fda6df

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
@@ -6,6 +7,9 @@ extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
+  //ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
+  int fd = fs_open(filename, 0, 0);
+  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd)); 
+  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }

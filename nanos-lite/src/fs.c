8e2613872df189bbfa29775cd7225263f43f997c
tracer-ics2017 <tracer@njuics.org>
2018-06-07 11:09:06 +0000

    >  run 161620202 Yee Linux 51f024b662be 4.9.60-linuxkit-aufs #1 SMP Mon Nov 6 16:00:12 UTC 2017 x86_64 GNU/Linux  11:09:06 up  2:56,  1 user,  load average: 0.04, 0.05, 0.00 a0c76944e4c02562ae19cb8bb43e8d9854add184

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -38,7 +38,7 @@ int fs_open(const char *pathname, int flags, int mode){
 	for(int i = 0; i < NR_FILES; i++){
 		if(strcmp(file_table[i].name, pathname) == 0){
 			file_table[i].open_offset = 0;
-			Log("in the open, filename:%s, fd = %d, file size = %d, file open_offset = %d, NR_FILES: %d\n",pathname, i, file_table[i].size, file_table[i].open_offset,NR_FILES);
+			//Log("in the open, filename:%s, fd = %d, file size = %d, file open_offset = %d, NR_FILES: %d\n",pathname, i, file_table[i].size, file_table[i].open_offset,NR_FILES);
 			return i;
 		}
 	}
@@ -48,7 +48,7 @@ int fs_open(const char *pathname, int flags, int mode){
 
 ssize_t fs_read(int fd, void *buf, size_t len){
     ssize_t size , rlen;
-	Log("in the read, fd = %d, file size = %d, len = %d, file open_offset = %d\n", fd, file_table[fd].size, len, file_table[fd].open_offset);
+	//Log("in the read, fd = %d, file size = %d, len = %d, file open_offset = %d\n", fd, file_table[fd].size, len, file_table[fd].open_offset);
     size = file_table[fd].size - file_table[fd].open_offset;
     rlen = len > size ? size : len;
     switch (fd) {
@@ -72,7 +72,7 @@ ssize_t fs_read(int fd, void *buf, size_t len){
 
 ssize_t fs_write(int fd, uint8_t *buf, size_t len){
     ssize_t size , wlen;
-	Log("in the write, fd = %d, file size = %d, len = %d, file open_offset = %d\n", fd, file_table[fd].size, len, file_table[fd].open_offset);
+	//Log("in the write, fd = %d, file size = %d, len = %d, file open_offset = %d\n", fd, file_table[fd].size, len, file_table[fd].open_offset);
     size = file_table[fd].size - file_table[fd].open_offset;
     wlen = len > size ? size : len;
     switch (fd){
@@ -95,10 +95,12 @@ ssize_t fs_write(int fd, uint8_t *buf, size_t len){
 }
 
 off_t fs_lseek(int fd, off_t offset, int whence){
+	Log("fd = %d, NR_FILES = %d, whence = ",fd,NR_FILES);
 	if(fd > NR_FILES)
 		return 0;
 	switch(whence){
 		case SEEK_SET:
+			Log("1");
 			if(offset >= 0 && offset <= file_table[fd].size){
 				file_table[fd].open_offset = offset;
 				return file_table[fd].open_offset;
@@ -106,12 +108,15 @@ off_t fs_lseek(int fd, off_t offset, int whence){
 			else
 				return -1;
 		case SEEK_CUR:
+			Log("2");
 			file_table[fd].open_offset += offset;
 			break;
 		case SEEK_END:
+			Log("3");
 			file_table[fd].open_offset += offset;
 			break;
 		default:
+			Log("4");
 			return -1;
 	}
 	if(file_table[fd].open_offset < 0 || file_table[fd].open_offset > file_table[fd].size)
